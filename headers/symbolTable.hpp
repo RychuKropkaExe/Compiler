1b83d0279d75da10afad591c3f9aafea6c92e277
RychuKropkaExe <jozek.melanczuk@gmail.com>
2023-09-22 19:00:02 +0200

    Init commit

---
+++ headers/symbolTable.hpp
@@ -0,0 +1,4 @@
+#ifndef SYMBOL_TABLE_H
+#define SYMBOL_TABLE_H
+    #define SIEMA_ENIU 2137
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include "../headers/symbolTable.hpp"
+#include <iostream>
+
+int main(){
+    std::cout << "Siema " << SIEMA_ENIU << "\n";
+}
