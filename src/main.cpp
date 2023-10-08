a43acdf2cd482b48fa4d1d5e587dea8095697115
RychuKropkaExe <jozek.melanczuk@gmail.com>
2023-10-08 18:43:24 +0200

    Setup lexer and parser

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
 #include "../headers/symbolTable.hpp"
 #include <iostream>
+#include <list>
+#include <string>
 
-int main(){
-    std::cout << "Siema ENIU" << SIEMA_ENIU << "\n";
+extern void run_parser(FILE *file, std::list<std::string> test);
+
+int main(int argc, char** argv){
+
+    if(argc < 2){
+        printf("No path provided\n");
+        return 0;
+    }
+
+    run_parser(NULL, std::list<std::string>());
+    
+
 }
